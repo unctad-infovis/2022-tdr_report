30a086c33bad8e23f5a045ed63644c3fc6165021
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-02 12:36:52 +0200

    Improved video autoplay

--- src/jsx/Video.jsx
+++ src/jsx/Video.jsx
@@ -5,13 +5,20 @@ function Video() {
   useEffect(() => {
     videoRef.current.src = videoRef.current.offsetWidth < 768 ? './assets/vid/2022-tdr_report_banner_animation_11.mp4' : './assets/vid/2022-tdr_report_banner_animation_169.mp4';
     videoRef.current.poster = videoRef.current.offsetWidth < 768 ? './assets/img/2022-tdr_report_banner_animation_11.jpg' : './assets/img/2022-tdr_report_banner_animation_169.jpg';
+    if (!videoRef.current.playing) {
+      videoRef.current.play();
+    }
   }, []);
   return (
     <>
       <div className="video_container">
-        <video src="" autoPlay muted ref={videoRef} poster="">
+        {
+        // eslint-disable-next-line
+        <video src=""  autoPlay muted playsInline ref={videoRef} poster="">
           <track default kind="captions" srcLang="en" src="" />
         </video>
+        }
+
       </div>
       <noscript>Your browser does not support JavaScript!</noscript>
     </>
