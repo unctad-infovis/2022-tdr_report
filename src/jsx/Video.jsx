2ec87f624c90e6691b25d1d3a467e88c401278bd
Teemo Tebest <teemo.tebest@gmail.com>
2022-11-22 12:19:30 +0100

    Updated URLs

--- src/jsx/Video.jsx
+++ src/jsx/Video.jsx
@@ -5,10 +5,10 @@ function Video() {
   const mp4Ref = useRef();
   const webmRef = useRef();
   useEffect(() => {
-    videoRef.current.src = (window.location.href.includes('unctad') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.mp4' : 'assets/vid/2022-tdr_report_banner_animation_169.mp4');
-    mp4Ref.current.src = (window.location.href.includes('unctad') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.mp4' : 'assets/vid/2022-tdr_report_banner_animation_169.mp4');
-    webmRef.current.src = (window.location.href.includes('unctad') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.webm' : 'assets/vid/2022-tdr_report_banner_animation_169.webm');
-    videoRef.current.poster = (window.location.href.includes('unctad') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/img/2022-tdr_report_banner_animation_11.jpg' : 'assets/img/2022-tdr_report_banner_animation_169.jpg');
+    videoRef.current.src = (window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.mp4' : 'assets/vid/2022-tdr_report_banner_animation_169.mp4');
+    mp4Ref.current.src = (window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.mp4' : 'assets/vid/2022-tdr_report_banner_animation_169.mp4');
+    webmRef.current.src = (window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/vid/2022-tdr_report_banner_animation_11.webm' : 'assets/vid/2022-tdr_report_banner_animation_169.webm');
+    videoRef.current.poster = (window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-tdr_report/' : './') + ((videoRef.current.offsetWidth < 768) ? 'assets/img/2022-tdr_report_banner_animation_11.jpg' : 'assets/img/2022-tdr_report_banner_animation_169.jpg');
     if (!videoRef.current.playing) {
       videoRef.current.play();
     }
