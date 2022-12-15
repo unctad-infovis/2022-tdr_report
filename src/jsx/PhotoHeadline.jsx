6c1a047c2a54239be322b509dcbc748f30b9e911
Teemo Tebest <teemo.tebest@gmail.com>
2022-12-15 09:59:14 +0100

    Fixed image url

--- src/jsx/PhotoHeadline.jsx
+++ src/jsx/PhotoHeadline.jsx
@@ -5,7 +5,7 @@ function PhotoHeadline({
   img, max_width, text_lower, text_upper
 }) {
   return (
-    <div className="photo_headline_container" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', backgroundImage: `url(${window.location.href.includes('unctad') ? 'https://storage.unctad.org/2022-tdr_report/' : './'}assets/img/${img})` }}>
+    <div className="photo_headline_container" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', backgroundImage: `url(${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-tdr_report/' : './'}assets/img/${img})` }}>
       <div className="photo_headline_content">
         <div className="photo_headlines">
           <h2 className="" style={{ maxWidth: max_width }}>{text_upper}</h2>
